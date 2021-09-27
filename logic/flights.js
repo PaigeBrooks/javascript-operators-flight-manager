06ddbee46440345552de8e11bd874120563228d2
PaigeBrooks <pbrooks@sterlingsolutions.co.uk>
2021-09-27 13:12:36 +0100

    flights=0

--- logic/flights.js
+++ logic/flights.js
@@ -1,6 +1,6 @@
 function Flights() {
 function calculateNumberOfFlights(passengers, capacity){
-    let flights;
+    let flights=0;
     if (passengers%capacity==0){
         flights = passengers/capacity;
     } else {
