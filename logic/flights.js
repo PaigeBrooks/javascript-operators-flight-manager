e38b2eb1005714250c38a6b6a9eb1780992b9029
PaigeBrooks <pbrooks@sterlingsolutions.co.uk>
2021-09-27 13:47:24 +0100

    Changed - to +

--- logic/flights.js
+++ logic/flights.js
@@ -3,7 +3,7 @@ function Flights() {
         if(passengers%capacity==0) {
             return passengers/capacity;
         } else {
-            return (passengers/capacity)-1;
+            return (passengers/capacity)+1;
         }
     }
     return {calculateNumberOfFlights};
