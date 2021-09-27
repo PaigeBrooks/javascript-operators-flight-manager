bdc41fb7d619553afeb1c9dc86d713aca168e2b0
PaigeBrooks <pbrooks@sterlingsolutions.co.uk>
2021-09-27 13:38:45 +0100

    copied whole section incl. error handlers

--- logic/flights.js
+++ logic/flights.js
@@ -1,11 +1,20 @@
 function Flights() {
     function calculateNumberOfFlights(passengers, capacity) {
-        let flights=0;
+        let flights;
+
+        if ((passengers < 0) || (!Number.isInteger(Number(passengers)))) {
+            throw new Error("The number of passengers must be a positive integer value")
+        }
+
+        if ((capacity < 0) || (!Number.isInteger(Number(capacity)))) {
+            throw new Error("The capacity of the flight must be a positive integer value")
+        }
+
         if (passengers % capacity == 0) {
             flights = passengers/capacity;
         } else {
             flights = Math.floor(passengers/capacity) + 1;
         }
-        return {calculateNumberOfFlights};
+        return flights;
     }
 module.exports = Flights();
