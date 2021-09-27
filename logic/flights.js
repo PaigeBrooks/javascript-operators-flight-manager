b1a643dac6701f316bd057f17631b23abf47a4cc
PaigeBrooks <pbrooks@sterlingsolutions.co.uk>
2021-09-27 13:58:29 +0100

    Added exception error

--- logic/flights.js
+++ logic/flights.js
@@ -1,5 +1,12 @@
 function Flights() {
     function calculateNumberOfFlights(passengers, capacity){
+        if (passengers<0){
+            throw new Error("The number of passengers must be a positive integer value");
+        }
+        if (capacity<0){
+            throw new Error("The capacity of the flight must be a positive integer value")
+        }
+        
         if(passengers%capacity==0) {
             return passengers/capacity;
         } else {
