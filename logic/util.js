81b99c20f68613ae3bfe107593e5dbbf82f76ba5
PaigeBrooks <pbrooks@sterlingsolutions.co.uk>
2021-09-28 11:48:43 +0100

    calculation functions in util.js

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,24 @@
 function Util() {
+function calculateTotalDistributedPassengers(distributedPassengers){
+    let totalDistributedPassengers = 0;
+    let value;
 
+    for (value in distributedPassengers){
+        totalDistributedPassengers += distributedPassengers[value];
+    }
+
+    return totalDistributedPassengers;
 }
 
+function calculateTotalNumberOfPassengers(passengerArray){
+    let calculateTotalNumberOfPassengers = 0;
+    let passengers;
+
+    for (passengers of passengerArray){
+        totalNumberOfPassengers += passengers;
+    }
+    return totalNumberOfPassengers;
+}
+return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
+}
+module.exports = Util();
